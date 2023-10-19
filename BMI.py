7df68d7607c9bed300b446f338c4f614ba9908e5
Mohammad Hossein <habibpour.programming@gmail.com>
2023-10-19 06:21:05 -0700

    color Fixed

--- BMI.py
+++ BMI.py
@@ -1,5 +1,9 @@
+from colorama import Fore
+
 def BMI(Weight , height):
     result = Weight / (height ** 2)
-    return result * 10000
+    return f"{Fore.BLUE}  {result * 10000}"
 
 print(BMI(79 ,184))
+
+# Mohammad Hossein Habibpour
