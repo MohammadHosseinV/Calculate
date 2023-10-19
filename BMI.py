dd01d7e08adde16f4bdf82221f099ce28c192e11
Mohammad Hossein <habibpour.programming@gmail.com>
2023-10-19 06:25:34 -0700

    Imput seted V2

--- BMI.py
+++ BMI.py
@@ -1,9 +1,12 @@
 from colorama import Fore
 
 def BMI(Weight , height):
+
     result = Weight / (height ** 2)
     return f"{Fore.BLUE}  {result * 10000}"
 
+Weight = int(input("Please enter your Weight :"))
+height = int(input("Please enter your height :"))
 print(BMI(79 ,184))
 
 # Mohammad Hossein Habibpour
